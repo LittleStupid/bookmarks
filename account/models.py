13c9a2d6cfa80078f5a998ff65b5c8a76ad4513d
LittleStupid <lttlstpd@live.com>
2017-01-13 13:35:19 +0800

    add follow system

--- account/models.py
+++ account/models.py
@@ -7,13 +7,13 @@ class Profile(models.Model):
     user = models.OneToOneField(settings.AUTH_USER_MODEL)
     date_of_birth = models.DateField(blank=True, null=True)
     photo = models.ImageField(upload_to='users/%Y/%m/%d', blank=True)
-    
+
     def __str__(self):
         return 'Profile for user {}'.format(self.user.username)
 
 
 class Contact(models.Model):
-    user_from = models.ForeignKey(User,related_name='rel_from_set')
+    user_from = models.ForeignKey(User, related_name='rel_from_set')
     user_to = models.ForeignKey(User, related_name='rel_to_set')
     created = models.DateTimeField(auto_now_add=True, db_index=True)
 
@@ -25,4 +25,5 @@ class Contact(models.Model):
 
 
 # Add following field to User dynamically
-User.add_to_class('following', models.ManyToManyField('self', through=Contact, related_name='followers', symmetrical=False))
+User.add_to_class('following', models.ManyToManyField(
+    'self', through=Contact, related_name='followers', symmetrical=False))
--- account/views.py
+++ account/views.py
@@ -5,6 +5,12 @@ from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from .forms import LoginForm, UserRegistrationForm, UserEditForm, ProfileEditForm
 from .models import Profile
+from django.shortcuts import get_object_or_404
+from django.contrib.auth.models import User
+from django.http import JsonResponse
+from django.views.decorators.http import require_POST
+from common.decorators import ajax_required
+from .models import Contact
 
 
 def user_login(request):
@@ -12,7 +18,8 @@ def user_login(request):
         form = LoginForm(request.POST)
         if form.is_valid():
             cd = form.cleaned_data
-            user = authenticate(username=cd['username'], password=cd['password'])
+            user = authenticate(
+                username=cd['username'], password=cd['password'])
             if user is not None:
                 if user.is_active:
                     login(request, user)
@@ -71,3 +78,42 @@ def edit(request):
 @login_required
 def dashboard(request):
     return render(request, 'account/dashboard.html', {'section': 'dashboard'})
+
+
+@login_required
+def user_list(request):
+    users = User.objects.filter(is_active=True)
+    return render(request,
+                  'account/user/list.html',
+                  {'section': 'people',
+                   'users': users})
+
+
+@login_required
+def user_detail(request, username):
+    user = get_object_or_404(User, username=username, is_active=True)
+    return render(request, 'account/user/detail.html', {'section': 'people', 'user': user})
+
+
+@ajax_required
+@require_POST
+@login_required
+def user_follow(request):
+    user_id = request.POST.get('id')
+    action = request.POST.get('action')
+    if user_id and action:
+        try:
+            user = User.objects.get(id=user_id)
+            if action == 'follow':
+                Contact.objects.get_or_create(
+                    user_from=request.user,
+                    user_to=user
+                )
+            else:
+                Contact.objects.filter(
+                    user_form=request.user,
+                    user_to=user).delete()
+            return JsonResponse({'status': 'ok'})
+        except User.DoesNotExist:
+            return JsonResponse({'status': 'ko'})
+    return JsonResponse({'status': 'ko'})
